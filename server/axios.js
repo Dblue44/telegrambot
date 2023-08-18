7fcb492d3a7e68cf05918e35f03ed0ddb14e5745
Dmitrii <stakheev12@mail.ru>
2023-08-18 15:28:30 +0500

    DEBUG

--- server/axios.js
+++ server/axios.js
@@ -4,20 +4,15 @@ axios.defaults.headers.common["Accept"] = "application/json";
 axios.defaults.headers.common["Content-Type"] = "application/json; charset=utf-8";
 axios.defaults.withCredentials = true
 
-let instance = axios.create({
+const instance = axios.create({
     baseURL: "https://dev-freshmarket.cloudbpm.ru",
 });
 
 export const setNewAxios = (Cookies, bpmcsrf) => {
-    instance = axios.create({
-        baseURL: "https://dev-freshmarket.cloudbpm.ru",
-        headers: {
-            "ForceUseSession": true,
-            "Accept": "application/json",
-            "Content-Type": "application/json; charset=utf-8",
-            "Cookie": Cookies,
-            "BPMCSRF": bpmcsrf
-        }
-    });
+    instance.interceptors.request.use((config) =>{
+        config.headers.set("Cookie", Cookies);
+        config.headers.set("BPMCSRF", bpmcsrf);
+        return config;
+    })
 };
 export default instance;
