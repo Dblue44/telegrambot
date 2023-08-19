78c61890a856c733ca99384647be96c2db3699a4
Dmitrii <stakheev12@mail.ru>
2023-08-19 16:13:18 +0500

    Deploy version 1.1.1
    (cherry picked from commit 62abb78ef3ef76bfe19a09e328d58c346cfcce18)
    
    # Conflicts:
    #	src/Components/Menu/Menu.js

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,40 +1,53 @@
 import React from "react";
 import Button from "@mui/material/Button";
-import { useSelector } from "react-redux";
-import { checkIsAuth } from "../../Redux/features/auth/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, checkIsAuth } from "../../Redux/features/auth/authSlice";
 
 import Container from "@mui/material/Container";
 import { Link } from "react-router-dom";
 import styles from "./Menu.module.scss";
 
 const Menu = () => {
-    //const dispatch = useDispatch();
+    const dispatch = useDispatch();
     const isAuth = useSelector(checkIsAuth);
 
-    // const onClickLogout = () => {
-    //     if (window.confirm("Вы действительно хотите выйти?")) {
-    //         dispatch(logout());
-    //         window.localStorage.removeItem("bpmcsrf");
-    //     }
-    // };
+    const onClickLogout = () => {
+        if (window.confirm("Вы действительно хотите выйти?")) {
+            dispatch(logout());
+            window.localStorage.removeItem("bpmcsrf");
+        }
+    };
 
     return (
-        <Container fixed sx={{paddingTop: "1em", paddingBottom: "1em"}}>
-            <div className={styles.inner}>
-                <Link className={styles.logo} to="/">
-                    <div>Супермаркет ДА!</div>
-                </Link>
-                <div className={styles.buttons}>
-                    {!isAuth &&
-                        <>
-                            <Link to="/login">
-                                <Button variant="outlined">Войти</Button>
-                            </Link>
-                        </>
-                    }
+        <div className={styles.Menu}>
+            <Container fixed>
+                <div className={styles.inner}>
+                    <Link className={styles.logo} to="/">
+                        <div>Creatio</div>
+                    </Link>
+                    <div className={styles.buttons}>
+                        {isAuth ? (
+                            <>
+                                <Button
+                                    onClick={onClickLogout}
+                                    variant="contained"
+                                    color="error"
+                                >
+                                    Выйти
+                                </Button>
+                            </>
+                        ) : (
+                            <>
+                                {/* eslint-disable-next-line react/jsx-no-undef */}
+                                <Link to="/login">
+                                    <Button variant="outlined">Войти</Button>
+                                </Link>
+                            </>
+                        )}
+                    </div>
                 </div>
-            </div>
-        </Container>
+            </Container>
+        </div>
     );
 };
 
